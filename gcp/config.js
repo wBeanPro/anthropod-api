6225a9b984f0b9642e14f153a3bcd1887dcd42cb
woobinpro <woobinpro@gmail.com>
2022-08-08 22:54:43 +0100

    Update config.js

--- gcp/config.js
+++ gcp/config.js
@@ -2,10 +2,10 @@ const { Storage } = require('@google-cloud/storage');
 const path = require('path');
 const fs = require('fs');
 
-const serviceKey = path.join(__dirname, '../earnest-beacon-358103-c65ec311e6e3.json'); 
+// const serviceKey = path.join(__dirname, '../earnest-beacon-358103-c65ec311e6e3.json'); 
 
 const storage = new Storage({
-    keyFilename: serviceKey,
+//     keyFilename: serviceKey,
     projectId: process.env.GCP_PROJECT_ID
 })
 
