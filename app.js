8515980a8f60d017d45bb0df12e942f88ab985ed
woobinpro <woobinpro@gmail.com>
2022-12-13 12:00:28 -0300

    updated for error

--- app.js
+++ app.js
@@ -1,59 +1,64 @@
-
-const path = require('path')
-require('dotenv').config({path:path.resolve(__dirname, '.env')});
-const express = require('express');
-const mongoose = require('mongoose');
-const {  ServerApiVersion, MongoClient } = require('mongodb');
-const cors =  require('cors');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const routes = require('./routes/index.js');
-const { read } = require('fs');
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, ".env") });
+const express = require("express");
+const mongoose = require("mongoose");
+const { ServerApiVersion, MongoClient } = require("mongodb");
+const cors = require("cors");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const routes = require("./routes/index.js");
+const { read } = require("fs");
 const app = express();
-const uri = `mongodb+srv://anthropoduser:${process.env.MONGO_PASSWORD}@cluster0.turudph.mongodb.net/anthropod?retryWrites=true&w=majority`
-const gcp = require('./gcp/config.js');
-
+const uri = `mongodb+srv://anthropoduser:${process.env.MONGO_PASSWORD}@cluster0.turudph.mongodb.net/anthropod?retryWrites=true&w=majority`;
+const gcp = require("./gcp/config.js");
 
 //mongodb database connection
-mongoose.connect(uri,{ useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1})
-.then(() => console.log('database is connected'))
-.catch(err => console.log(err))
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  })
+  .then(() => console.log("database is connected"))
+  .catch((err) => console.log(err));
 
-morgan.token('host',(req,res) =>{
-	return req.hostname;
-})
+morgan.token("host", (req, res) => {
+  return req.hostname;
+});
 
-gcp.getBuckets().then((x) => console.log(x))
+gcp.getBuckets().then((x) => console.log("Buckets are fetched"));
 //middleware
 
 // cors options
-const allowedOrigins  = ['http://localhost:4200', 'https://anthropod-staging.herokuapp.com','https://anthropod-r7lsnsohxq-uc.a.run.app']
+const allowedOrigins = [
+  "http://localhost:4200",
+  "https://anthropod-staging.herokuapp.com",
+  "https://anthropod-r7lsnsohxq-uc.a.run.app",
+];
 const corsOptions = {
-    origin: (origin, cb) => {
-        if(!origin || allowedOrigins.indexOf(origin) !== -1){
-            cb(null, true)
-        } else [
-            cb(new Error('not allowed by cors!'))
-        ]
-    },
-    optionsSuccessStatus: 200
-}
+  origin: (origin, cb) => {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      cb(null, true);
+    } else [cb(new Error("not allowed by cors!"))];
+  },
+  optionsSuccessStatus: 200,
+};
 
-app.use(morgan(':method :host :status :res[content-type] - :response-time ms'));
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json({limit: '10mb'}));
+app.use(morgan(":method :host :status :res[content-type] - :response-time ms"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "10mb" }));
 app.use(cors(corsOptions));
 
-app.use(function(req, res, next){
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-methods', 'GET, POST, PUT, DELETE')
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    
-    next();
-})
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Credentials", true);
 
-app.use('/api', routes);
+  next();
+});
 
-app.listen(process.env.PORT,() => {
-    console.log(`APP IS RUNNING ON PORT: ${process.env.PORT}`)
-})
+app.use("/api", routes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`APP IS RUNNING ON PORT: ${process.env.PORT}`);
+});
--- routes/song.js
+++ routes/song.js
@@ -1,12 +1,25 @@
-const router = require('express').Router();
-const songController = require('../controllers/song.js');
-const { Upload_image_or_audio } = require('../middleware/file_middleware');
-const { verifyToken } = require('../middleware/auth.js');
+const router = require("express").Router();
+const songController = require("../controllers/song.js");
+const { Upload_image_or_audio } = require("../middleware/file_middleware");
+const { verifyToken } = require("../middleware/auth.js");
 
-
-router.get('/', songController.get_all_songs);
-router.get('/:id',songController.get_song_by_id)
-router.get('/user/:id', songController.get_songs_by_user_id);
-router.post('/' ,[verifyToken, Upload_image_or_audio.fields([{name:'cover', maxCount: 1}, {name:'song', maxCount:1}])], songController.create_song)
-router.put('/:id/edit_song',[verifyToken, Upload_image_or_audio.single('song')], songController.update_song);
+router.get("/", songController.get_all_songs);
+router.get("/:id", songController.get_song_by_id);
+router.get("/user/:id", songController.get_songs_by_user_id);
+router.post(
+  "/",
+  [
+    verifyToken,
+    Upload_image_or_audio.fields([
+      { name: "cover", maxCount: 1 },
+      { name: "song", maxCount: 1 },
+    ]),
+  ],
+  songController.create_song
+);
+router.put(
+  "/:id/edit_song",
+  [verifyToken, Upload_image_or_audio.single("song")],
+  songController.update_song
+);
 module.exports = router;
