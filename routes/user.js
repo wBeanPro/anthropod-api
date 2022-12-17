cc8a79c2bfec241c34ed987994e9b688a9bb4adb
woobinpro <woobinpro@gmail.com>
2022-12-17 13:50:30 -0300

    temp

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,14 @@
-const router = require('express').Router();
-const auth = require('../middleware/auth.js');
-const User_Controller = require('../controllers/user.js');
-const { UploadImage } = require('../middleware/file_middleware.js');
+const router = require("express").Router();
+const auth = require("../middleware/auth.js");
+const User_Controller = require("../controllers/user.js");
+const { UploadImage } = require("../middleware/file_middleware.js");
 
+router.get("/:userId", auth.verifyToken, User_Controller.getUser);
 
-
-router.get('/:userId', auth.verifyToken, User_Controller.getUser)
-
-router.put('/:id/edit', [auth.verifyToken, UploadImage.single('avatar')],User_Controller.updateUser)
+router.put(
+  "/:id/edit",
+  [auth.verifyToken, UploadImage.single("avatar")],
+  User_Controller.updateUser
+);
 
 module.exports = router;
