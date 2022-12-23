40f39fe5663392b9da4cea3bb9d6bc166252a29d
woobinpro <woobinpro@gmail.com>
2022-12-23 14:48:06 -0300

    update api for art page

--- controllers/art.js
+++ controllers/art.js
@@ -42,6 +42,59 @@ exports.CREATE_ART = async (req, res, next) => {
   }
 };
 
+exports.UPDATE_COMMENT = async (req, res, next) => {
+  try {
+    ArtModel.findByIdAndUpdate(req.body.id, {
+      $push: { comments: [req.userId] },
+    }).exec((err, art) => {
+      if (err)
+        return res
+          .status(403)
+          .send({ isSuccess: FontFaceSetLoadEvent, message: err.message });
+      else {
+        return res.send({ isSuccess: true, comments: art.comments });
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(403).send({ isSuccess: false, message: err.message });
+  }
+};
+
+exports.LIKE_ART = async (req, res, next) => {
+  try {
+    console.log("SONG liKED", req.body.id);
+    ArtModel.findById(req.body.id).exec((err, art) => {
+      const checkArr = art.likes.filter((userId) => {
+        if (userId.toString() === req.userId) {
+          return true;
+        }
+        return false;
+      });
+
+      if (checkArr.length > 0) {
+        console.log("already liked");
+        return res.send({ isSuccess: false, message: "liked" });
+      }
+
+      ArtModel.findByIdAndUpdate(req.body.id, {
+        $push: { likes: [req.userId] },
+      }).exec((err, art) => {
+        if (err)
+          return res
+            .status(403)
+            .send({ isSuccess: false, message: err.message });
+        else {
+          return res.send({ isSuccess: true, likes: art.likes });
+        }
+      });
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(403).send({ isSuccess: false, message: err.message });
+  }
+};
+
 exports.UPDATE_ART = async (req, res, next) => {
   const { id } = req.params;
   const { name, description } = req.body;
@@ -63,12 +116,10 @@ exports.UPDATE_ART = async (req, res, next) => {
       if (err)
         return res.status(500).send({ isSuccess: false, message: err.message });
       else if (updatedItem) {
-        return res
-          .status(200)
-          .send({
-            isSuccess: true,
-            message: `${updatedItem.name} has succesfully updated!`,
-          });
+        return res.status(200).send({
+          isSuccess: true,
+          message: `${updatedItem.name} has succesfully updated!`,
+        });
       }
     }
   );
